90c44b1111939aff87c850a68e22dbdabb6dccb7
Suraj Yadav <surajyadavy676@gmail.com>
2024-06-27 12:01:01 +0530

    adding animation in main body

--- src/components/HomeBody.jsx
+++ src/components/HomeBody.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { PiImagesThin } from "react-icons/pi";
+import { TbBulb } from "react-icons/tb";
+import { TbWorld } from "react-icons/tb";
 import "animate.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function HomeBody() {
+  useEffect(() => {
+    AOS.init({
+      // Global settings:
+      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+      initClassName: 'aos-init', // class applied after initialization
+      animatedClassName: 'aos-animate', // class applied on animation
+      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+      // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+      offset: 120, // offset (in px) from the original trigger point
+      delay: 0, // values from 0 to 3000, with step 50ms
+      duration: 400, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: false, // whether elements should animate out while scrolling past them
+      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    });
+  }, []);
   return (
     <div>
       <div className="bg-gradient-to-r from-violet-500 to-fuchsia-500">
@@ -19,7 +46,7 @@ function HomeBody() {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 my-24 gap-10 w-[80%] mx-auto text-xl items-center">
-        <div className="font-semibold">
+        <div className="font-semibold" data-aos="flip-left">
           <p className="my-5">Ready to Elevate Your Customer Service?</p>
           <p>
             Discover how SimpliAssist can transform your call center operations
@@ -29,31 +56,46 @@ function HomeBody() {
             Learn More
           </button>
         </div>
-        <div>
-          <div>
-            <p className="font-bold">
-              100% Automation of In-Bound Call Operations
-            </p>
-            <p className="font-light">
-              SimpliAssist redefines customer service efficiency by automating
-              inbound call center operations entirely.
-            </p>
+        <div data-aos="flip-right" className="animate__bounceInRight">
+          <div className="flex items-center">
+            <span className="mr-3 bg-green-800 p-2 rounded-full">
+              <TbWorld className="text-white font-bold text-2xl" />
+            </span>
+            <div>
+              <p className="font-bold">
+                100% Automation of In-Bound Call Operations
+              </p>
+              <p className="font-light">
+                SimpliAssist redefines customer service efficiency by automating
+                inbound call center operations entirely.
+              </p>
+            </div>
           </div>
-          <div className="my-9">
-            <p className="font-bold">Seamless Integration and deployment</p>
-            <p className="font-light">
-              Our platform flawlessly integrates with your existing CRM systems
-              and prioritize simplicity and security in the deployment of our
-              AI-led solutions.
-            </p>
+          <div className="my-9 flex items-center">
+            <span className="mr-3 bg-green-800 p-2 rounded-full">
+              <TbBulb className="text-white font-bold text-2xl" />
+            </span>
+            <div>
+              <p className="font-bold">Seamless Integration and deployment</p>
+              <p className="font-light">
+                Our platform flawlessly integrates with your existing CRM
+                systems and prioritize simplicity and security in the deployment
+                of our AI-led solutions.
+              </p>
+            </div>
           </div>
-          <div>
-            <p className="font-bold">ROI-Driven Approach</p>
-            <p className="font-light">
-              Every aspect of SimpliAssist is designed to deliver measurable
-              business impact - from improving customer satisfaction to
-              significantly enhancing operational efficiency.
-            </p>
+          <div className="flex items-center">
+            <span className="mr-3 bg-green-800 p-2 rounded-full">
+              <PiImagesThin className="text-white font-bold text-2xl" />
+            </span>
+            <div>
+              <p className="font-bold">ROI-Driven Approach</p>
+              <p className="font-light">
+                Every aspect of SimpliAssist is designed to deliver measurable
+                business impact - from improving customer satisfaction to
+                significantly enhancing operational efficiency.
+              </p>
+            </div>
           </div>
         </div>
       </div>
